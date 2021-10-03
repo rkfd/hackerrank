6dde0e7ccda4e15060c601edf22aa5aa9dc19a2e
Satwik Gawand <satwikgawand@gmail.com>
2021-10-03 14:25:22 +0530

    add: diagonal-difference

---
+++ problem-solving/diagonal-difference.py
@@ -0,0 +1,23 @@
+def diagonalDifference(arr):
+  # code starts here
+  n = len(arr)
+  ltor = 0
+  rtol = 0
+  
+  for i in range(0,n):
+    ltor += arr[i][i]
+    rtol += arr[i][n-i-1]
+
+  return abs(ltor-rtol)
+  # code ends here
+
+
+# input
+input_array = [
+  [1,2,3],
+  [4,5,6],
+  [9,8,9]
+]
+
+# output
+print(f'Diagonal Difference: {diagonalDifference(input_array)}')
