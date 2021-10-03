2f7c8592f7020c957b57d86d4b8a97dc8713a316
Satwik Gawand <satwikgawand@gmail.com>
2021-10-03 13:48:42 +0530

    new: solve-me-first and simple-array-sum

---
+++ problem-solving/simple-array-sum.py
@@ -0,0 +1,8 @@
+def simpleArraySum(ar):
+    # code starts here
+    return sum(ar)
+    # code ends here
+
+
+input_array = [1,3,2,6,3]
+print(f'Sum of array: {simpleArraySum(input_array)}')
---
+++ problem-solving/solve-me-first.py
@@ -0,0 +1,9 @@
+def solveMeFirst(a,b):
+  # code starts here
+  return a+b
+  # code ends here
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
