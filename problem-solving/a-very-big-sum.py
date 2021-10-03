8234bce1206f3461e8dd3ef3b2b35257dd9fdc2e
Satwik Gawand <satwikgawand@gmail.com>
2021-10-03 14:08:11 +0530

    add: a-very-big-sum

---
+++ problem-solving/a-very-big-sum.py
@@ -0,0 +1,11 @@
+def aVeryBigSum(ar):
+  # code starts here
+  return sum(ar)
+  # code ends here
+
+
+# input
+input_array = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+# output
+print(f'A Very Big Sum: {aVeryBigSum(input_array)}')
