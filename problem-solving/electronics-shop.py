d70a3ec6a26a4b130b1c604c3d74287d5b138698
Satwik Gawand <satwikgawand@gmail.com>
2021-10-04 22:14:13 +0530

    add: electronics-shop

---
+++ problem-solving/electronics-shop.py
@@ -0,0 +1,27 @@
+import time
+
+
+def getMoneySpent(keyboards, drives, b):
+  # code starts here
+  max = i = j = 0
+  
+  for k in range(0,len(keyboards)):
+    for d in range(0,len(drives)):
+      if (keyboards[k] + drives[d]) > max and (keyboards[k] + drives[d] <= b):
+        max = keyboards[k] + drives[d]
+
+  if max == 0:
+    return -1
+  else:
+    return max
+  # code ends here
+
+
+# input
+keyboards = [3, 1]
+drives = [5, 2, 8]
+b = 10
+
+# output
+start = time.time()
+print(getMoneySpent(keyboards, drives, b))
