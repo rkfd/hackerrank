eeeb14828cacfcaa4ec2a658302dc606b7e31d91
Satwik Gawand <satwikgawand@gmail.com>
2021-10-04 19:14:15 +0530

    add: grading-students

---
+++ problem-solving/grading-students.py
@@ -0,0 +1,24 @@
+import time
+import math
+
+
+def gradingStudents(grades):
+    results = []
+    for grade in grades:
+        multiple = math.ceil(grade/5) * 5
+        if grade < 38 or (multiple - grade) >= 3:
+            results.append(grade)
+        elif (multiple - grade) < 3:
+            results.append(multiple)
+            
+    return results
+
+
+# input
+input_array = [4,73,67,38,33]
+
+# output
+start = time.time()
+print(gradingStudents(input_array))
+end = time.time()
+print(f'Time Elapsed: {abs(end-start)}')
